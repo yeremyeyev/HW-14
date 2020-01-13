7b67218f3dccad544a3a6d22eee058320a4cdbd8
Eremeev Andery <a.yeremyeyev@gmail.com>
2020-01-13 22:46:30 +0200

    Update ExchangeRates.js
    украсил

--- ExchangeRates.js
+++ ExchangeRates.js
@@ -5,8 +5,63 @@ let obj;
 
 request(url, function(error, response, body) {
   obj = JSON.parse(body);
-  console.log(obj[0].base_ccy + "-" + obj[0].ccy + "=" + obj[0].buy);
-  console.log(obj[1].base_ccy + "-" + obj[1].ccy + "=" + obj[1].buy);
-  console.log(obj[2].base_ccy + "-" + obj[2].ccy + "=" + obj[2].buy);
-  console.log(obj[3].base_ccy + "-" + obj[3].ccy + "=" + obj[3].buy);
+  console.log("Курсы валют" + "\n");
+  console.log(
+    "Гривна к Евро" +
+      " " +
+      obj[0].base_ccy +
+      "-" +
+      obj[0].ccy +
+      " " +
+      "\n" +
+      "Покупка" +
+      " " +
+      obj[0].buy
+  );
+  console.log("Продажа" + " " + obj[0].sale);
+
+  console.log(
+    "\n" +
+      "Гривна к Рубль" +
+      " " +
+      obj[1].base_ccy +
+      "-" +
+      obj[1].ccy +
+      " " +
+      "\n" +
+      "Покупка" +
+      " " +
+      obj[1].buy
+  );
+  console.log("Продажа" + " " + obj[1].sale + "\n");
+
+  console.log(
+    "\n" +
+      "Гривна к Доллар" +
+      " " +
+      obj[2].base_ccy +
+      "-" +
+      obj[2].ccy +
+      " " +
+      "\n" +
+      "Покупка" +
+      " " +
+      obj[2].buy
+  );
+  console.log("Продажа" + " " + obj[2].sale + "\n");
+
+  console.log(
+    "\n" +
+      "Гривна к Биткоин" +
+      " " +
+      obj[3].base_ccy +
+      "-" +
+      obj[3].ccy +
+      " " +
+      "\n" +
+      "Покупка" +
+      " " +
+      obj[3].buy
+  );
+  console.log("Продажа" + " " + obj[3].sale + "\n");
 });
